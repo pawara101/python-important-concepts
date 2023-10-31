fc5785b2a77790364a58f03ec67832c8f5264e94
pawara101 <pawara101dassanayake@gmail.com>
2023-10-31 23:03:41 +0530

    threading part 1

--- Threading/thread2.py
+++ Threading/thread2.py
@@ -1,1 +1,15 @@
 ## Code with threading
+import threading
+import time
+
+start = time.perf_counter()
+def doSomething():
+    print('Sleep 1 sec')
+    time.sleep(1)
+    print('Done')
+
+doSomething()
+doSomething()
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 3)} secs')
