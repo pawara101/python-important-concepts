2b977c30211065a96b8c928416b692623e721fe8
pawara101 <pawara101dassanayake@gmail.com>
2023-11-04 10:23:40 +0530

    Multi processing v2.1

--- Threading/multiproc.py
+++ Threading/multiproc.py
@@ -2,22 +2,30 @@
 import time
 import multiprocessing
 
+## context
+import concurrent.futures
+
 start = time.perf_counter()
 
 def do_something(tm):
     print(f'Sleep {tm} second')
     time.sleep(tm)
-    print('Done Sleeping....')
+    return 'Done sleep'
 
-process =[]
-for _ in range(10):
-    p = multiprocessing.Process(target=do_something,args=[1.5])
-    p.start()
-    process.append(p)
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    f1 = executor.submit(do_something,1)
+    f2 = executor.submit(do_something,2)
+    print(f1.result())
+    print(f2.result())
 
-for procs in process:
-    procs.join()
+# process =[]
+# for _ in range(10):
+#     p = multiprocessing.Process(target=do_something,args=[1.5])
+#     p.start()
+#     process.append(p)
 
+# for procs in process:
+#     procs.join()
 
 finish = time.perf_counter()
 
