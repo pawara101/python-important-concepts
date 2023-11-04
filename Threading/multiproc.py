d43759732c8bc25ba038d3de5e234680aae9ebab
pawara101 <pawara101dassanayake@gmail.com>
2023-11-04 10:32:19 +0530

    Multi processing v2.4

--- Threading/multiproc.py
+++ Threading/multiproc.py
@@ -14,10 +14,12 @@ def do_something(tm):
 
 with concurrent.futures.ProcessPoolExecutor() as executor:
     secs = [5,4,3,2,1]
-    results = [executor.submit(do_something,sec)for sec in secs]
+    results = executor.map(do_something,secs)
 
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
+    for result in results:
+        print(result)
+
+
 # process =[]
 # for _ in range(10):
 #     p = multiprocessing.Process(target=do_something,args=[1.5])
